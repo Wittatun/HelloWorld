98f5b4c54f742dc3818f148261bc53b83c90f8b6
Wittatun <cwc9328@outlook.com>
2018-11-22 22:42:21 +0800

    test
    test

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.button1.Click += Button1_Click;
+            //CheckRun();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //CheckRun();
+        }
+
+        private static void CheckRun()
+        {
+
+            bool isapprunning = false;
+            System.Threading.Mutex mutex = new System.Threading.Mutex(true, System.Diagnostics.Process.GetCurrentProcess().ProcessName,
+            out isapprunning);
+            if (!isapprunning)
+            {
+                MessageBox.Show("本程序已经在运行了，请不要重复运行！");
+                Environment.Exit(1);
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+        }
+    }
+}
